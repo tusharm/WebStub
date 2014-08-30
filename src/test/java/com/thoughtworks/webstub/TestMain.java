7131493e2de3bf8196851be4bf7a81d63c69c254
TusharM <tushar.madhukar@gmail.com>
2014-08-30 21:59:02 +0530

    Removed redundant test class

--- src/test/java/com/thoughtworks/webstub/TestMain.java
+++
@@ -1,7 +0,0 @@
-package com.thoughtworks.webstub;
-
-public class TestMain {
-    public static void main(String[] args) throws Exception {
-//        StubServer.at(9009).withContext().withContext().start();
-    }
-}
